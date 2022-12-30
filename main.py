d6b06b34897b6b3d7e61e830e81ededd0d24de6f
acsavag20 <acsavag@gmail.com>
2022-12-30 14:53:05 -0500

    Update main.py
    changed tactics to upload excel to git repo

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask,render_template
+import openpyxl
 import requests,re,pandas as pd
+import time
+import git
+import openpyxl
 app = Flask(__name__)
+
 
 @app.route("/About Me")
 def about():
@@ -13,16 +18,22 @@ def about():
 #url = "https://www.owgr.com/current-world-ranking"
 
 
-@app.route('/Golf')
-def ranking():
-    # Obtain the data and extract the table.
-    resp = requests.get('https://www.pgatour.com/stats/stat.186.html')
-    df = pd.read_html(resp.content)[1]
-    # Rename the specified columns and extract them into a new DataFrame.
-    data = df.rename(columns={"RANK\xa0THIS WEEK":"Rank","PLAYER NAME":"Name"})[['Rank', 'Name']]
-    # Pass all local variables to the template by name.
-    return render_template('Golf/rankings.html', **locals())
-
 if __name__ == "__main__":
     app.run(debug=True)
 
+
+resp = requests.get('https://www.pgatour.com/stats/stat.186.html')
+df = pd.read_html(resp.content)[1]
+# Rename the specified columns and extract them into a new DataFrame.
+df = df.rename(columns={"RANK\xa0THIS WEEK":"Rank","PLAYER NAME":"Name"})[['Rank', 'Name']]
+df['Update Time']=pd.Timestamp.now()
+
+repo = git.Repo("")
+workbook= openpyxl.Workbook()
+sheet= workbook.active
+sheet = df
+workbook.save("wgr_rankings.xlsx")
+repo.index.add(["wgr_rankings.xlsx"])
+repo.index.commit("Added golfers excel")
+origin=repo.remote("origin")
+origin.push()
